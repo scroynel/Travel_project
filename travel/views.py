6df2972ff6e3bf4d94506bc49385cb614aed44c6
Vladyslav <scroynel@gmail.com>
2026-06-15 18:59:18 +0200

    Creating viewsets for 2 models

--- travel/views.py
+++ travel/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, exceptions
+from .models import TravelProject, ProjectPlace
+from .serializer import TravelProjectSerializer, ProjectPlaceSerializer
 
-# Create your views here.
+
+class TraveProjectViewSet(viewsets.ModelViewSet):
+    queryset = TravelProject.objects.all()
+    serializer_class = TravelProjectSerializer
+
+
+class ProjectPlaceViewSet(viewsets.ModelViewSet):
+    queryset = ProjectPlace.objects.all()
+    serializer_class = ProjectPlaceSerializer
